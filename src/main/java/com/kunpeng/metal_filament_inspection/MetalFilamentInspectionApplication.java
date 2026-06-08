1c3d51db329857282139519cf61ac12ece74085b
huanhongyam <shendie402170@126.com>
2026-06-08 16:31:36 +0800

    first commit

---
+++ src/main/java/com/kunpeng/metal_filament_inspection/MetalFilamentInspectionApplication.java
@@ -0,0 +1,13 @@
+package com.kunpeng.metal_filament_inspection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalFilamentInspectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetalFilamentInspectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kunpeng/metal_filament_inspection/MetalFilamentInspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunpeng.metal_filament_inspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalFilamentInspectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
